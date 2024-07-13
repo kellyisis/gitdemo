5858931225912021650b6e06b94493668cd4707a
Kelly <kellywu292655@gmail.com>
2024-07-13 14:52:52 +0800

    "加了第幾次"

--- game.py
+++ game.py
@@ -2,8 +2,16 @@ import random
 
 x = random.randint(1, 50)
 print(x)
-y = int(input("請猜一個數字"))
-if x == y:
-    print("猜對了!")
-else:
-    print("猜錯了~~")
+for i in range(5):
+    y = int(input("請猜一個數字"))
+    if x == y:
+        print("猜對了!")
+        break
+    else:
+        if x > y:
+            print("猜高一點")
+        else:
+            print("猜低一點")
+
+if x != y:
+    print(f"答案為:{x}")
