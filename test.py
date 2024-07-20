01c3ba1e31c104ee73846e452baa970ae8c5bc96
Kelly <kellywu292655@gmail.com>
2024-07-20 10:08:33 +0800

    修改一

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-x = 10
-y = 20
-z = x * y
+import random
 
-print(z)
+x = random.randint(1, 100)
+print(x)
+for i in range(15):
+    y = int(input("請猜一個數字"))
+    if x == y:
+        print("猜對了!")
+        break
+    else:
+        if x > y:
+            print("猜高一點")
+        else:
+            print("猜低一點")
+
+if x != y:
+    print(f"答案為:{x}")
+else:
+    print(f"總共猜了{i+1}次")
